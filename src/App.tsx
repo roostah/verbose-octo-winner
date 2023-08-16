a4e33fd693c5a67ac4bfb4bed096c76f9d5ab65a
Josh Mahoney <josh@mahoney.com>
2023-08-16 15:29:11 -0400

    initial app setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useFormik } from "formik";
+import { Button } from "@chakra-ui/react";
+import { useRef } from "react";
 
-function App() {
+export default function App() {
+  const fileRef = useRef(null);
+  
+  const formik = useFormik({
+    initialValues: {
+      file: '',
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2))
+    },
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor='file'>Select file to hash</label>
+      <input
+        id='file'
+        type='file'
+        ref={fileRef}
+        {...formik.getFieldProps('file')}
+      />
+      {formik.errors.file ? <div>{formik.errors.file}</div> : null}
+      <Button type='submit'>Submit</Button>
+    </form>
+  )
 }
-
-export default App;
