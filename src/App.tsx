fe0ee1e31dfcdd7b9fa1500b3b43a5d97577f97c
Josh Mahoney <josh@mahoney.com>
2023-08-17 00:54:25 -0400

    basic file meta display

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import { useFormik } from "formik";
-import { Button } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Flex,
+  HStack,
+  Heading,
+  Stack,
+  StackDivider,
+  Text,
+  Textarea,
+  VStack } from "@chakra-ui/react";
 import { useEffect, useMemo, useState, useRef } from "react";
 import { processList } from "./util/enums";
 import { hashFileInput } from "./util/hashfileinput";
-//import { getFileHash } from "./util/FileHash";
 
 
 export interface FormValues {
   file: string;
+  filedescription: string;
 };
 
 const App: React.FC = () => {
@@ -17,7 +30,13 @@ const App: React.FC = () => {
   );
 
   const fileRef = useRef<HTMLInputElement>(null);
-
+  const [filemeta, setFileMeta] = useState({
+    hash: '',
+    name: '',
+    size: 0,
+    description: ''
+  });
+  
   useEffect(() => {
     if (window.Worker) {
       fhWorker.postMessage(processList.create);
@@ -26,59 +45,108 @@ const App: React.FC = () => {
 
 
   const handleSubmitFile = (values: FormValues) => {
-    console.log(values);
     if (fileRef && fileRef.current && fileRef.current.files) {
       let file = fileRef.current.files[0];
-      console.log(file.size);
 
       if (window.Worker) {
         hashFileInput(
           file,
           fhWorker,
           (chunks, chunksProcessed) => {
-            console.log('progress: ', chunksProcessed, chunks,)
+            console.log('progress: ', chunksProcessed, chunks);
           },
           (hash, timeProcessed) => {
             console.log('finished: ', hash, timeProcessed);
+            setFileMeta({
+              ...filemeta,
+              hash: hash,
+              name: file.name,
+              size: file.size,
+              description: values.filedescription
+            });
           });
       }
-      
-      let reader = new FileReader();
-      reader.onload = (event) => {
-        if (!event.target) {
-          return;
-        }
-        let data = event.target.result;
-        if (typeof(data) !== 'string') {
-          return;
-        }
-        console.log(data);
-        let encrypted = 'foo';// getFileHash(data);
-        console.log('encrypted: ' + encrypted);
-      };
-      reader.readAsBinaryString(file);
     }
   };
   
   const formik = useFormik({
     initialValues: {
-      file: ''
+      file: '',
+      filedescription: ''
     },
     onSubmit: handleSubmitFile,
   });
 
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor='file'>Select file to hash</label>
-      <input
-        id='file'
-        type='file'
-        ref={fileRef}
-        {...formik.getFieldProps('file')}
-      />
-      {formik.errors.file ? <div>{formik.errors.file}</div> : null}
-      <Button type='submit'>Submit</Button>
-    </form>
+    <Flex bg="gray.100" align="center" justify="center" h="100vh">
+      <HStack alignItems={"stretch"}>
+      <Box bg="white" p={6} rounded="md">
+        <form onSubmit={formik.handleSubmit}>
+          <VStack spacing={4} align="flex-start">
+            <label htmlFor='file'>Select file to hash</label>
+            <input
+              id='file'
+              type='file'
+              ref={fileRef}
+              {...formik.getFieldProps('file')}
+            />
+
+            <Textarea
+              id='filedescription'
+              placeholder='Enter file description'
+              maxLength={500}
+              {...formik.getFieldProps('filedescription')}
+             />
+
+            {formik.errors.file ? <div>{formik.errors.file}</div> : null}
+            <Button type='submit'>Submit</Button>
+          </VStack>
+        </form>
+      </Box>
+      <Card size={"lg"}>
+        <CardHeader>
+          <Heading size='md'>File Hash Report</Heading>
+        </CardHeader>
+
+        <CardBody>
+          <Stack divider={<StackDivider />} spacing='4'>
+            <Box>
+              <Heading size='xs' textTransform='uppercase'>
+                SHA256
+              </Heading>
+              <Text pt='2' fontSize='sm' color="green">
+                {filemeta.hash}
+              </Text>
+            </Box>
+            <Box>
+              <Heading size='xs' textTransform='uppercase'>
+                File name
+              </Heading>
+              <Text pt='2' fontSize='sm'>
+                {filemeta.name}
+              </Text>
+            </Box>
+            <Box>
+              <Heading size='xs' textTransform='uppercase'>
+                File Size
+              </Heading>
+              <Text pt='2' fontSize='sm'>
+                {filemeta.size}
+              </Text>
+            </Box>
+            <Box>
+              <Heading size='xs' textTransform='uppercase'>
+                File Description
+              </Heading>
+              <Text pt='2' fontSize='sm'>
+                {filemeta.description}
+              </Text>
+            </Box>
+          </Stack>
+        </CardBody>
+      </Card>
+      </HStack>
+    </Flex>
   )
 }
 
